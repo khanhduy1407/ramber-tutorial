9f5c94d5643dfc20beb47a16a181c33fe5103110
NKDuy <nkduy.dev@gmail.com>
2025-10-22 09:50:06 +0700

    POST Requests

--- src/server.js
+++ src/server.js
@@ -1,17 +1,19 @@
-import sirv from 'sirv';
-import polka from 'polka';
-import compression from 'compression';
-import * as ramber from '@ramber/server';
+import sirv from "sirv";
+import polka from "polka";
+import compression from "compression";
+import * as ramber from "@ramber/server";
+import { json } from "body-parser";
 
 const { PORT, NODE_ENV } = process.env;
-const dev = NODE_ENV === 'development';
+const dev = NODE_ENV === "development";
 
 polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		ramber.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+  .use(
+    json(),
+    compression({ threshold: 0 }),
+    sirv("static", { dev }),
+    ramber.middleware()
+  )
+  .listen(PORT, (err) => {
+    if (err) console.log("error", err);
+  });
